cd5b0bc4e58db8803b5d0968fa3b07855c1f9af5
Preston Hill <preston.leon.hill@gmail.com>
2022-11-04 23:00:32 -0500

    added html docs for creating base handlebars templates for testing, added user-routes, configured routes differently, added style.css

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
 
 app.use(session({secret: "keyboard cat", resave: false, saveUninitialized: true, store: new SequelizeStore({db: sequelize,})}));
 app.engine('handlebars', hbs.engine);
@@ -14,7 +15,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public/')));
-app.use(require('./controllers'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
